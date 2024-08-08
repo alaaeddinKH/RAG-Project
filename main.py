d48f9ce2a5e99cd0b326e4798188079b91d5ecbf
ALAAEDDİN  KHEİTİ <103529192+alaaeddinKH@users.noreply.github.com>
2024-08-09 00:05:28 +0300

    update main file

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+load_dotenv(".env")
+from routes import base
+
 
 app = FastAPI()
+app.include_router(base.base_router)
 
-@app.get("/welcome")
-def welcome():
-    return {"message":"Hello world!"}
+
