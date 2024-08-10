2231030adb908898539a728472b1c2c6f4b0a166
ALAAEDDİN  KHEİTİ <103529192+alaaeddinKH@users.noreply.github.com>
2024-08-11 01:12:50 +0300

    update main fiile

--- src/main.py
+++ src/main.py
@@ -1,8 +1,5 @@
 from fastapi import FastAPI
-from dotenv import load_dotenv
-load_dotenv(".env")
-from routes import base
-
+from src.routes import base
 
 app = FastAPI()
 app.include_router(base.base_router)
