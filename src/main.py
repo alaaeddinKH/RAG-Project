f5271dc5cc545354b216e25accd041f96474d562
ALAAEDDİN  KHEİTİ <103529192+alaaeddinKH@users.noreply.github.com>
2024-08-24 18:00:07 +0300

    update

--- src/main.py
+++ src/main.py
@@ -5,3 +5,4 @@ app = FastAPI()
 app.include_router(base.base_router)
 app.include_router(data.data_router)
 
+
--- src/routes/base.py
+++ src/routes/base.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI, APIRouter, Depends
 from src.helpers.config import get_settings, Settings
 
-
 base_router = APIRouter(
     prefix="/api/mini", tags=["api"]
 )  # this parameter to add words before all of routes and to add tags 
@@ -12,3 +11,5 @@ async def welcome(app_settings: Settings = Depends(get_settings)):
     app_version = app_settings.APP_VERSION
     return {"app_name":app_name,
             "app_version": app_version,}
+
+
