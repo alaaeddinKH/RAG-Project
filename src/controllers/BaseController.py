ae1b2af7609f1bf7d77f0994304afe4c9af0198c
ALAAEDDİN  KHEİTİ <103529192+alaaeddinKH@users.noreply.github.com>
2024-08-24 22:26:58 +0300

    add some detalies about projectcontroler to base

--- src/controllers/BaseController.py
+++ src/controllers/BaseController.py
@@ -1,8 +1,14 @@
 from src.helpers.config import get_settings, Settings
+import os 
 
 class BaseController:
     
     def __init__(self):
         self.app_settings = get_settings()
+        self.base_dir = os.path.dirname(os.path.dirname(__file__))
+        self.file_dir = os.path.join(
+            self.base_dir,
+            "asset/files"
+        )
 
     
