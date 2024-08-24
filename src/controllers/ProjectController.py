7c689bdf71b2835c6f4447015b7e6780156fa593
ALAAEDDİN  KHEİTİ <103529192+alaaeddinKH@users.noreply.github.com>
2024-08-24 22:27:12 +0300

    create project controller file

---
+++ src/controllers/ProjectController.py
@@ -0,0 +1,19 @@
+from .BaseController import BaseController
+from fastapi import UploadFile
+from src.models import ResponseSignal
+import os
+
+class ProjectControler(BaseController):
+    def __init__(self):
+        super().__init__()
+
+    def get_project_path(self, projec_id: str):
+        project_dir = os.path.join(
+            self.file_dir,
+            projec_id
+        )
+
+        if not os.path.exists(project_dir):
+            os.makedirs(project_dir)
+        
+        return project_dir
