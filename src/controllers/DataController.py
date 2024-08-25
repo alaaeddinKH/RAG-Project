c70462b7a2d2233371279a0207e8f4f7b01c858c
ALAAEDDİN  KHEİTİ <103529192+alaaeddinKH@users.noreply.github.com>
2024-08-25 15:15:42 +0300

    add func to make projct file and to generate name of file

--- src/controllers/DataController.py
+++ src/controllers/DataController.py
@@ -1,6 +1,9 @@
 from .BaseController import BaseController
 from fastapi import UploadFile
 from src.models import ResponseSignal
+from .ProjectController import ProjectControler
+import os
+import re
 
 class DataController(BaseController):
     
@@ -17,4 +20,35 @@ class DataController(BaseController):
 
         return True, ResponseSignal.FILE_UPLOAD_SUCCESS.value
 
+    def generate_unique_filepath(self, orig_file_name: str, project_id: str):
 
+        random_key = self.generate_random_string()
+        project_path = ProjectControler().get_project_path(project_id=project_id)
+
+        cleaned_file_name = self.get_clean_file_name(
+            orig_file_name=orig_file_name
+        )
+
+        new_file_path = os.path.join(
+            project_path,
+            random_key + "_" + cleaned_file_name
+        )
+
+        while os.path.exists(new_file_path):
+            random_key = self.generate_random_string()
+            new_file_path = os.path.join(
+                project_path,
+                random_key + "_" + cleaned_file_name
+            )
+
+        return new_file_path, random_key + "_" + cleaned_file_name
+
+    def get_clean_file_name(self, orig_file_name: str):
+
+        # remove any special characters, except underscore and .
+        cleaned_file_name = re.sub(r'[^\w.]', '', orig_file_name.strip())
+
+        # replace spaces with underscore
+        cleaned_file_name = cleaned_file_name.replace(" ", "_")
+
+        return cleaned_file_name
