b9ff90730252b2d0af0a470b58e7a136ce378ea1
ALAAEDDİN  KHEİTİ <103529192+alaaeddinKH@users.noreply.github.com>
2024-09-13 00:27:13 +0300

    create schemes to project

---
+++ src/models/db_schemes/project.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel, Field, validator
+from typing import Optional
+from bson.objectid import ObjectId
+
+class Project(BaseModel):
+    _id : Optional[ObjectId]
+    project_id: str = Field(..., min_length=1)
+
+    @validator("project_id")
+    def validate_project_id(cls, value):
+        if not value.isalnum():
+            raise ValueError("project id must be alphanumaric")
+        
+        return value
+
+
+    class Config:   # to allow when take unknown value
+        arbitrary_types_allowed = True
+
+
+        
